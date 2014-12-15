5f534ff9f1f0de16cbaaaa0217b20e12b4d317df
Mark J Pritt <mjpritt@gradx.cs.jhu.edu>
2014-12-15 17:19:47 -0500

    Improved style

--- public_html/top_users.php
+++ public_html/top_users.php
@@ -9,10 +9,11 @@
     $attr = $_POST['attr'];
     $lim = $_POST['limit'];
 
-    $sql    = 'SELECT CONCAT(FName, \' \' ,LName) as Name, Industry FROM User ORDER BY ' . $attr;
+    $sql    = 'SELECT CONCAT(FName, \' \' ,LName) as Name, Industry, NumConnection, Age FROM User ORDER BY ' . $attr;
     if ($_POST['order'] == 'DESC')
         $sql = $sql . ' DESC';
     $sql = $sql . ' LIMIT 0,' . $lim;
+    
     $result = mysql_query($sql, $conn);
 
     // check if the query successfully executed
@@ -23,14 +24,14 @@
     }
     
     // show results
-    echo '<h3>Most Popular Users</h3>';
-    
-    echo '<table border=1>';
-    echo '<tr> <th>Name</th> <th>Industry</th></tr>';
+    echo '<table class="results">';
+    echo '<tr> <th>Name</th> <th>Industry</th> <th>Num Connections</th> <th>Age</th></tr>';
     while ($row = mysql_fetch_assoc($result)) {
         echo '<tr>' ;
         echo '<td>' . $row['Name'] .  '</td>' ;
         echo '<td>' . $row['Industry'] .  '</td>';
+        echo '<td class="numeric">' . $row['NumConnection'] . '</td>';
+        echo '<td class="numeric">' . $row['Age'] . '</td>';
         echo '</tr>';
     }
     echo '</table>';
