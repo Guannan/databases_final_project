0384b67b10f900db51432926b6e03886c8c15483
Guannan <gren3@jhu.edu>
2014-12-16 11:08:35 -0800

    Fixed key duplication for tables.

--- linkedin_extraction/test.rb
+++ linkedin_extraction/test.rb
@@ -3,22 +3,34 @@
 require 'mechanize'
 
 links_arr = []
+count = 1
 File.readlines('links.txt').each do |line|
+	count += 1
 	if "#{line}".chomp != ""
 		# puts "#{line}"
 		links_arr = links_arr + [line]		
+	end
+
+	if count == 3
+		break 
 	end
 end
 
 USER_AGENTS = ['Windows IE 6', 'Windows IE 7', 'Windows Mozilla', 'Mac Safari', 'Mac FireFox', 'Mac Mozilla', 'Linux Mozilla', 'Linux Firefox', 'Linux Konqueror']
 
 @user_id = 0
-@university_id = 100
-@degree_id = 200
-@skill_id = 300
-@group_id = 400
-@language_id = 500
-@employer_id = 600
+@university_id = {}
+@university_id_mark = 100
+@degree_id = {}
+@degree_id_mark = 200
+@skill_id = {}
+@skill_id_mark = 300
+@group_id = {}
+@group_id_mark = 400
+@language_id = {}
+@language_id_mark = 500
+@employer_id = {}
+@employer_id_mark = 600
 
 def http_client
   	Mechanize.new do |agent|
@@ -52,27 +64,27 @@ def generate_user_id
 end
 
 def generate_university_id
-	@university_id = @university_id + 1
+	@university_id_mark += 1
 end
 
 def generate_degree_id
-	@degree_id = @degree_id + 1
+	@degree_id_mark += 1
 end
 
 def generate_skill_id
-	@skill_id = @skill_id + 1
+	@skill_id_mark += 1
 end
 
 def generate_group_id
-	@group_id = @group_id + 1
+	@group_id_mark += 1
 end
 
 def generate_language_id
-	@language_id = @language_id + 1
+	@language_id_mark += 1
 end
 
 def generate_employer_id
-	@employer_id = @employer_id + 1
+	@employer_id_mark += 1
 end
 
 def parse_skills(mech)
@@ -97,40 +109,60 @@ def generate_sql(mech)
 
 	education = mech.search('.background-education .education').map do |item|
 		university_name   = item.at('h4').text.gsub(/\s+|\n/, ' ').strip if item.at('h4')
-		desc   = item.at('h5').text.gsub(/\s+|\n/, ' ').strip if item.at('h5')
+		degree_name   = item.at('h5').text.gsub(/\s+|\n/, ' ').strip if item.at('h5')
 		period = item.at('.education-date').text.gsub(/\s+|\n/, ' ').strip if item.at('.education-date')
 
-		generate_university_id
-		generate_degree_id
-		puts "insert into Education values ( #{@user_id} , #{@university_id} , #{@degree_id} , '#{desc}', '#{period}', '#{period}');"
-		puts "insert into University values ( #{@university_id} , '#{university_name}');"	
+		if !@university_id.has_key?(university_name)
+			@university_id[university_name] = @university_id_mark
+			generate_university_id
+		end
+
+		if !@degree_id.has_key?(degree_name)
+			@degree_id[degree_name] = @degree_id_mark
+			generate_degree_id
+		end
+		puts "insert into Education values ( #{@user_id} , #{@university_id[university_name]} , #{@degree_id[degree_name]} , '#{degree_name}', '#{period}', '#{period}');"
+		puts "insert into University values ( #{@university_id[university_name]} , '#{university_name}');"	
 	end
 
 	endorsements = 0
 
 	skills.each do |skill|
-		generate_skill_id
-		puts "insert into Has_skill values ( #{@user_id} , #{@skill_id}, #{endorsements});"
-		puts "insert into Skill values ( #{@skill_id} , '#{skill}');"
+		
+		if !@skill_id.has_key?(skill)
+			@skill_id[skill] = @skill_id_mark
+			generate_skill_id
+		end
+
+		puts "insert into Has_skill values ( #{@user_id} , #{@skill_id[skill]}, #{endorsements});"
+		puts "insert into Skill values ( #{@skill_id[skill]} , '#{skill}');"
 	end
 
 	member_count = 0
 	groups = mech.search('.groups-name').map do |item|
 		group_name = item.text.gsub(/\s+|\n/, ' ').strip
-		group_link = "http://www.linkedin.com#{item.at('a')['href']}"
+		# group_link = "http://www.linkedin.com#{item.at('a')['href']}"
 
-		generate_group_id
-		puts "insert into Member_of values ( #{@user_id} , #{@group_id});"
-		puts "insert into Groups values ( #{@group_id} , '#{group_name}', #{member_count});"
+		if !@group_id.has_key?(group_name)
+			@group_id[group_name] = @group_id_mark
+			generate_group_id
+		end
+
+		puts "insert into Member_of values ( #{@user_id} , #{@group_id[group_name]});"
+		puts "insert into Groups values ( #{@group_id[group_name]} , '#{group_name}', #{member_count});"
 	end
 
 	languages = mech.search('.background-languages #languages ol li').map do |item|
 		language_name    = item.at('h4').text rescue nil
 		# proficiency = item.at('div.languages-proficiency').text.gsub(/\s+|\n/, ' ').strip rescue nil
 
-		generate_language_id
-		puts "insert into Knows_language values ( #{@user_id} , #{@language_id});"
-		puts "insert into Languages values ( #{@language_id} , '#{language_name}');"
+		if !@language_id.has_key?(language_name)
+			@language_id[language_name] = @language_id_mark
+			generate_language_id
+		end
+
+		puts "insert into Knows_language values ( #{@user_id} , #{@language_id[language_name]});"
+		puts "insert into Languages values ( #{@language_id[language_name]} , '#{language_name}');"
 	end
 
 	employments = []
@@ -147,6 +179,7 @@ def generate_sql(mech)
 		employments << employment
 		end
 	end
+
 	if mech.search(".background-experience .past-position").first
 		mech.search(".background-experience .past-position").each do |experience|
 
@@ -162,12 +195,17 @@ def generate_sql(mech)
 	end
 
 	employments.each do |employment|
-		generate_employer_id
+
+		if !@employer_id.has_key?(employment[:location])
+			@employer_id[employment[:location]] = @employer_id_mark
+			generate_employer_id
+		end
+
 		employer_name = employment[:location]
 		start_date = employment[:start_date]
 		end_date = employment[:end_date]
-		puts "insert into Employer values ( #{@employer_id} , '#{employer_name}');"
-		puts "insert into Experience values ( #{@user_id} , #{@employer_id}, '#{start_date}', '#{end_date}');"
+		puts "insert into Employer values ( #{@employer_id[employment[:location]]} , '#{employer_name}');"
+		puts "insert into Experience values ( #{@user_id} , #{@employer_id[employment[:location]]}, '#{start_date}', '#{end_date}');"
 	end
 end
 
@@ -177,14 +215,14 @@ links_arr.each do |profile_link|
 	# html= mech.body
 	generate_sql(mech)
 
-	visitor_links = mech.search('.insights-browse-map/ul/li').map do |visitor|
-		v = {}
-		v[:link]    = visitor.at('a')['href']
-		v[:name]    = visitor.at('h4/a').text
-		v[:title]   = visitor.at('.browse-map-title').text.gsub('...', ' ').split(' at ').first
-		v[:company] = visitor.at('.browse-map-title').text.gsub('...', ' ').split(' at ')[1]
-		v
-	end
+	# visitor_links = mech.search('.insights-browse-map/ul/li').map do |visitor|
+	# 	v = {}
+	# 	v[:link]    = visitor.at('a')['href']
+	# 	v[:name]    = visitor.at('h4/a').text
+	# 	v[:title]   = visitor.at('.browse-map-title').text.gsub('...', ' ').split(' at ').first
+	# 	v[:company] = visitor.at('.browse-map-title').text.gsub('...', ' ').split(' at ')[1]
+	# 	v
+	# end
 
 	delay_me
 end
