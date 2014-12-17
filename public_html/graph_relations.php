629888a9ff0f609c33f03fbbf3ca1e0c8914d323
Mark J Pritt <mjpritt@gradx.cs.jhu.edu>
2014-12-16 21:50:34 -0500

    Can now graph categorical and aggregations of variables.

--- public_html/graph_relations.php
+++ public_html/graph_relations.php
@@ -3,84 +3,27 @@
     include 'includes/open.php';
     include 'includes/header.php'; 
     include 'includes/constants.php';
-
-
-/**
-    //$join_conditions = array("User" => array("Has_skill" => "User.UserID=Has_skill.UserID", "Knows_Language" ));
-
-    // Create and execute the query
-    $x_name = $_POST['xvar'];
-    $x = $fields[$x_name];
-
-    $y_name = $_POST['yvar'];
-    $y = $fields[$y_name];
-
-    echo '<h1>' . $y_name . ' vs. ' . $x_name . '</h1>';
-    
-    $attr_string = $x[1] . ' AS ' . $x[2] . ',' . $y[1] . ' AS ' . $y[2];
-    
-    $table_string = $x[0];
-    $condition = '';
-    if ($x[0] != $y[0]) {
-        $condition = ' WHERE ' . $x[0] . '.UserID=' . $y[0] . '.UserID';
-
-        $table_string = $x[0] . ',' . $y[0];
-
-        //if (array_key_exists($x[0], $join_conditions) and array_key_exists($y[0], $join_conditions[$x[0]]))
-        //    $condition = ' WHERE ' . $join_conditions[$x[0]][$y[0]];
-        //else
-        //    $condition = ' WHERE ' . $join_conditions[$y[0]][$x[0]];
-    }
-
-    $sql    = 'SELECT ' . $attr_string . ' FROM ' . $table_string . $condition;
-    if (count($x) > 3) {
-        $sql = $sql . ' GROUP BY ' . $x[0] . '.' . $x[3];
-        if (count($y) > 3) { 
-            $sql = $sql . ',' . $y[0] . '.' . $y[3];
-        }
-    }
-    elseif (count($y) > 3) {
-        $sql = $sql . ' GROUP BY ' . $y[0] . '.' . $y[3];
-    }
-
-    $result = mysql_query($sql, $conn);
-
-    // check if the query successfully executed
-    if (!$result) {
-        echo "DB Error, could not query the database. :-( <br/>";
-        echo 'MySQL Error: ' . mysql_error() . '<br/>';
-        exit;
-    }
-
-
-    echo '<div id="data" style="display:none;">';
-    
-    $data = array();
-    while($row = mysql_fetch_assoc($result)) {
-        $data[] = array($x[2] => intval($row[$x[2]]), $y[2] => intval($row[$y[2]]));
-    }
-    echo json_encode($data);
-    echo '</div>';
-
-    echo '<div id="xname" style="display:none;">' . $x[2] . '</div>';
-    echo '<div id="yname" style="display:none;">' . $y[2] . '</div>';
-    echo '<div id="xlabel" style="display:none;">' . $x_name . '</div>';
-    echo '<div id="ylabel" style="display:none;">' . $y_name . '</div>';
-**/
 ?>
 
 <h1 id="title"></h1>
 <div id="canvas"></div>
 <div id="inputForm">
     <form name="ajaxform" id="ajaxform" action="get_relation_data.php" method="POST">
-        <?php echo 'Plot <select name="yvar">';
-            foreach ($fields as $f=>$g) {
+        Plot <select name="aggregation">
+            <option value="ALL">All</option>
+            <option value="AVG" selected="selected">Average</option>
+            <option value="MAX">Maximum</option>
+            <option value="MIN">Minimum</option>
+        </select>
+
+        <?php echo ' <select name="yvar">';
+            foreach ($continuous_vars as $f=>$g) {
                 echo '<option value="' . $f . '">' . $f . '</option>';
             }
             echo '</select>';
 
             echo ' as a function of <select name="xvar">';
-            foreach ($fields as $f=>$g) {
+            foreach (array_merge($categorical_vars, $continuous_vars) as $f=>$g) {
                 echo '<option value="' . $f . '">' . $f . '</option>';
             }
             echo '</select><br/><br/>';
@@ -106,11 +49,22 @@
         var xlabel = result.xlabel;
         var ylabel = result.ylabel;
 
+        // mode 0 = scatterplot
+        // mode 1 = bar chart
+        // mode 2 = line chart
+        var mode = result.mode;
+
         var w = $("#canvas").width();
         var h = $("#canvas").height();
 
         // top, bottom, left, right
         var padding = [30, 50, 60, 30];
+
+        // Bar chart parameters
+        var barPadding = 5;
+        var plotW = w - padding[2] - padding[3] - barPadding;
+        var barWidth = plotW / dataset.length;
+        barPadding = Math.min(5, barWidth/4);
 
         // Create SVG element
         var svg = d3.select("#canvas")
@@ -135,6 +89,70 @@
                       .orient("left")
                       .ticks(8);
 
+
+
+        // Draw axis labels
+        if (mode == 0 || mode == 2) {
+            svg.append("text")
+                .attr("class", "xlabel")
+                .attr("text-anchor", "middle")
+                .attr("x", w/2)
+                .attr("y", h-10)
+                .text(xlabel);
+        }
+        svg.append("text")
+            .attr("class", "ylabel")
+            .attr("text-anchor", "middle")
+            .attr("x", -h/2)
+            .attr("dy", "1.2em")
+            .attr("transform", "rotate(-90)")
+            .text(ylabel);
+
+        // Plot
+        if (mode == 0) {
+            // Plot data points
+            svg.selectAll("circle")
+                   .data(dataset)
+                   .enter()
+                   .append("circle")    
+                   .attr("class", "dot")
+                   .attr("cx", function(d) { return xScale(d[xname]); })
+                   .attr("cy", function(d) { return yScale(d[yname]); })
+                   .attr("r", 5);
+        }
+        else if (mode == 1) {
+            // Draw bars
+            var bar = svg.selectAll("g")
+                   .data(dataset)
+                   .enter().append("g")
+                   .attr("transform", function(d, i) { return "translate(" + (barPadding + padding[2] + i * barWidth) + ",0)"; });
+
+            bar.append("rect")
+                   .attr("y", function(d) { return yScale(d[yname]); })
+                   .attr("width", barWidth - barPadding)
+                   .attr("height", function(d) { return h-padding[1]-yScale(d[yname]); });
+            
+            // Draw labels
+            var angle = Math.atan(12 / barWidth) * 180 / Math.PI;  // Shallowest angle to avoid overlapping
+            bar.append("g")
+                .attr("class", "barLabel")
+                .attr("transform", function(d) { return "translate(" + (barWidth/8) + "," + (h-padding[1] + 3) + ")"; })
+                .append("text")
+                .attr("dy", "0.75em")
+                .attr("transform", "rotate("+angle+")")
+                .text(function(d) { return d[xname]; });
+        }
+        else if (mode == 2) {
+            line = d3.svg.line()
+                .x(function(d) { return xScale(d[xname]); })
+                .y(function(d) { return yScale(d[yname]); });
+
+            svg.append("path")
+                .datum(dataset)
+                .attr("class", "line")
+                .attr("d", line);
+        }
+            
         // Draw axes
         svg.append("g")
             .attr("class", "axis")
@@ -145,42 +163,15 @@
             .attr("transform", "translate(" + padding[2] + ",0)")
             .call(yAxis);
 
-        // Draw axis labels
-        svg.append("text")
-            .attr("class", "xlabel")
-            .attr("text-anchor", "middle")
-            .attr("x", w/2)
-            .attr("y", h-10)
-            .text(xlabel);
-        svg.append("text")
-            .attr("class", "ylabel")
-            .attr("text-anchor", "middle")
-            .attr("x", -h/2)
-            .attr("dy", "1.2em")
-            .attr("transform", "rotate(-90)")
-            .text(ylabel);
-
-        // Plot data points
-        svg.selectAll("circle")
-           .data(dataset)
-           .enter()
-           .append("circle")    
-           .attr("cx", function(d) { return xScale(d[xname]); })
-           .attr("cy", function(d) { return yScale(d[yname]); })
-           .attr("r", 5);
     } 
-    
+   
+
     // callback handler for form submit
     $("#ajaxform").submit(function(e)
     {
-        console.log($(this));
-
         var postData = $(this).serializeArray();
         var formURL = $(this).attr("action");
         
-        console.log(postData);
-        console.log(formURL);
-
         $.ajax(
         {
             url: formURL,
@@ -189,17 +180,19 @@
             success: function(result)
             {
                 resultParsed = JSON.parse(result);
+                console.log(resultParsed);
                 drawTitle(resultParsed);
                 drawGraph(resultParsed);
             },
             error: function(jqXHR, status, error)
             {
+                console.log('Error!');
                 console.log(jqXHR);
+                console.log(status);
                 console.log(error);
             }
         });
 
-        console.log('Done');
         e.preventDefault();
         //e.unbind();
     });
@@ -209,9 +202,6 @@
 </script>
 
 <?php
-    // flush
-    mysql_free_result($result);
-    
     include 'includes/footer.php';
 ?>
 
--- public_html/includes/constants.php
+++ public_html/includes/constants.php
@@ -3,17 +3,41 @@
      * Contains constants used by other pages
      */
 
+    $tables = array("User", "Education", "Degree", "University", "Experience", "Employer", "Member_of", 
+                    "Groups", "Has_skill", "Skill", "Knows_language", "Languages");
+    
+    // Attributes on which we can join these tables
+    $cross_refs = array(array(1=>"User.UserID=Education.UserID", 4=>"User.UserID=Experience.UserID", 6=>"User.UserID=Member_of.UserID",
+                              8=>"User.UserID=Has_skill.UserID", 10=>"User.UserID=Knows_language.UserID"),
+                        array(0=>"Education.UserID=User.UserID", 2=>"Education.DegreeID=Degree.DegreeID", 3=>"Education.UniversityID=University.UniversityID"),
+                        array(1=>"Degree.DegreeID=Education.DegreeID"),
+                        array(1=>"University.UniversityID=Education.UniversityID"),
+                        array(0=>"Experience.UserID=User.UserID", 5=>"Experience.EmployerID=Employer.EmployerID"),
+                        array(4=>"Employer.EmployerID=Experience.EmployerID"),
+                        array(0=>"Member_of.UserID=User.UserID", 7=>"Member_of.GroupID=Groups.GroupID"),
+                        array(6=>"Groups.GroupID=Member_of.GroupID"),
+                        array(0=>"Has_skill.UserID=User.UserID", 9=>"Has_skill.SkillID=Skill.SkillID"),
+                        array(8=>"Skill.SkillID=Has_skill.SkillID"),
+                        array(0=>"Knows_language.UserID=User.UserID", 11=>"Knows_language.LanguageID=Languages.LanguageID"),
+                        array(10=>"Languages.LanguageID=Knows_language.LanguageID"));
+
 
     /**
      * For accessing arbitrary pairs of relations. Each element is of the form:
-     *    English Name => array(Table, Select Term, Name of row in returned table, (Optional) Attribute to group on)
+     *    English Name => array(Select term, List of Tables, WHERE term, Attribute to group on)
      */
-    $fields = array("Num Connections" => array("User", "User.NumConnection", "NumConnection"),
-        "Age" => array("User", "User.Age", "Age"),
-        "Num Skills" => array("Has_skill", "COUNT(Has_skill.SkillID)", "NumSkills", "UserID"),
-        "Num Languages" => array("Knows_language", "COUNT(Knows_language.LanguageID)", "NumLanguages", "UserID"),
-        "Num Jobs" => array("Experience", "COUNT(Experience.EmployerID)", "NumJobs", "UserID")
+    $continuous_vars = array("Num Connections" => array("User.NumConnection", array(0), "", ""),
+        "Age" => array("User.Age", array(0), "", ""),
+        "Num Skills" => array("COUNT(Has_skill.SkillID)", array(8), "", "Has_skill.UserID"),
+        "Num Languages" => array("COUNT(Knows_language.LanguageID)", array(10), "", "Knows_language.UserID"),
+        "Num Jobs" => array("COUNT(Experience.EmployerID)", array(4), "", "Experience.UserID")
       );
 
+    $categorical_vars = array("Industry" => array("User.Industry", array(0), "", ""),
+        "University" => array("University.UniversityName", array(1,3), "", "")
+    );
+
+
+    //$aggregates = array("AVERAGE" => "AVERAGE"
 ?>
 
