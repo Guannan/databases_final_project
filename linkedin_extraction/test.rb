c84d1f741b1f113b5d6767f767af5919a8790de0
Guannan <gren3@jhu.edu>
2014-12-17 17:36:24 -0800

    Added unique user id identifiers. Rule out duplicated search links.

--- linkedin_extraction/test.rb
+++ linkedin_extraction/test.rb
@@ -2,10 +2,14 @@
 
 require 'mechanize'
 
+
+# output = File.open( "output.txt","w" )
+
 class RetrieveProfile
 
 	class << self; 
 		attr_accessor :user_id, 
+					  :user_id_mark,
 					  :university_id,
 					  :university_id_mark,
 					  :degree_id,
@@ -20,7 +24,8 @@ class RetrieveProfile
 					  :employer_id_mark
 	end
 
-	@user_id = 0
+	@user_id = {}
+	@user_id_mark = 0
 	@university_id = {}
 	@university_id_mark = 100
 	@degree_id = {}
@@ -62,7 +67,7 @@ class RetrieveProfile
 	end
 
 	def generate_user_id
-		RetrieveProfile.user_id += 1
+		RetrieveProfile.user_id_mark += 1
 	end
 
 	def generate_university_id
@@ -113,13 +118,21 @@ class RetrieveProfile
 	def generate_sql(mech)
 		first_name = parse_firstname(mech)
 		last_name = parse_lastname(mech)
+		full_name = first_name + ' ' + last_name
 		connections = parse_connections(mech)
 		industry = parse_industry(mech)
 		skills = parse_skills(mech)
 
-		generate_user_id
+		if !RetrieveProfile.user_id.has_key?(full_name)
+			RetrieveProfile.user_id[full_name] = RetrieveProfile.user_id_mark
+			generate_user_id
+		else
+			return
+		end
+
 		age = 0
-		puts "insert into User values ( #{RetrieveProfile.user_id} , '#{first_name}' , '#{last_name}', #{connections}, #{age}, '#{industry}');"
+		
+		puts "insert into User values ( #{RetrieveProfile.user_id[full_name]} , '#{first_name}' , '#{last_name}', #{connections}, #{age}, '#{industry}');"
 
 		education = mech.search('.background-education .education').map do |item|
 			university_name = item.at('h4').text.gsub(/\s+|\n/, ' ').strip if item.at('h4')
@@ -137,7 +150,7 @@ class RetrieveProfile
 				RetrieveProfile.degree_id[degree_name] = RetrieveProfile.degree_id_mark
 				generate_degree_id
 			end
-			puts "insert into Education values ( #{RetrieveProfile.user_id} , #{RetrieveProfile.university_id[university_name]} , #{RetrieveProfile.degree_id[degree_name]} , '#{degree_name}', '#{period}', '#{period}');"
+			puts "insert into Education values ( #{RetrieveProfile.user_id[full_name]} , #{RetrieveProfile.university_id[university_name]} , #{RetrieveProfile.degree_id[degree_name]} , '#{degree_name}', '#{period}', '#{period}');"
 			puts "insert into University values ( #{RetrieveProfile.university_id[university_name]} , '#{university_name}');"	
 		end
 
@@ -150,7 +163,7 @@ class RetrieveProfile
 				generate_skill_id
 			end
 
-			puts "insert into Has_skill values ( #{RetrieveProfile.user_id} , #{RetrieveProfile.skill_id[skill]}, #{endorsements});"
+			puts "insert into Has_skill values ( #{RetrieveProfile.user_id[full_name]} , #{RetrieveProfile.skill_id[skill]}, #{endorsements});"
 			puts "insert into Skill values ( #{RetrieveProfile.skill_id[skill]} , '#{skill}');"
 		end
 
@@ -164,7 +177,7 @@ class RetrieveProfile
 				generate_group_id
 			end
 
-			puts "insert into Member_of values ( #{RetrieveProfile.user_id} , #{RetrieveProfile.group_id[group_name]});"
+			puts "insert into Member_of values ( #{RetrieveProfile.user_id[full_name]} , #{RetrieveProfile.group_id[group_name]});"
 			puts "insert into Groups values ( #{RetrieveProfile.group_id[group_name]} , '#{group_name}', #{member_count});"
 		end
 
@@ -177,7 +190,7 @@ class RetrieveProfile
 				generate_language_id
 			end
 
-			puts "insert into Knows_language values ( #{RetrieveProfile.user_id} , #{RetrieveProfile.language_id[language_name]});"
+			puts "insert into Knows_language values ( #{RetrieveProfile.user_id[full_name]} , #{RetrieveProfile.language_id[language_name]});"
 			puts "insert into Languages values ( #{RetrieveProfile.language_id[language_name]} , '#{language_name}');"
 		end
 
@@ -221,7 +234,7 @@ class RetrieveProfile
 			start_date = employment[:start_date]
 			end_date = employment[:end_date]
 			puts "insert into Employer values ( #{RetrieveProfile.employer_id[employment[:location]]} , '#{employer_name}');"
-			puts "insert into Experience values ( #{RetrieveProfile.user_id} , #{RetrieveProfile.employer_id[employment[:location]]}, '#{start_date}', '#{end_date}');"
+			puts "insert into Experience values ( #{RetrieveProfile.user_id[full_name]} , #{RetrieveProfile.employer_id[employment[:location]]}, '#{start_date}', '#{end_date}');"
 		end
 	end
 
@@ -269,21 +282,25 @@ USER_AGENTS = [ 'Windows IE 6',
 @agent.max_history = 0
 
 profile = RetrieveProfile.new
+visited_links = {}
 
 while 1
 	if links_arr.size <= 0
 		break
 	else
 		profile_link = links_arr.pop
-		# puts profile_link
+		puts ''
+		puts profile_link
 		mech = @agent.get(profile_link)
 		profile.generate_sql(mech)	
+		visited_links[profile_link] = 1
 
 		visitor_links = mech.search('.insights-browse-map/ul/li').map do |visitor|
-			v = {}
-			v[:link] = visitor.at('a')['href']
-			links_arr << v[:link]
-		end			
+			link = visitor.at('a')['href']
+			if !visited_links.has_key?(link)
+				links_arr << link
+			end
+		end	
 	end
 	profile.delay_me
 end
@@ -294,5 +311,5 @@ end
 # end
 
 
-
+# output.close
 
